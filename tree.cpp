b96cbfe19c5515b26c265a235ced441297e21956
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2020-05-12 22:15:17 +0200

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -14,15 +14,24 @@ struct Node {
     int d_sum { 0 };
     int d_product { 1 };
     int d_length { 0 };
+    bool hidden { false };
     Node* parent { nullptr };
     Node* first_son { nullptr };
     Node* last_son { nullptr };
     Node* brother { nullptr };
 };
 
+inline bool is_leaf(Node *node) {
+    return node->value != -1;
+}
+
+inline bool is_leaf(Node &node) {
+    return node.value != -1;
+}
+
 struct Tree {
     int i { -1 };
-    int next_free { 0 };
+    int leafs { 0 };
     Node* root { nullptr };
 };
 
@@ -30,6 +39,30 @@ struct Tree {
 Node* TREES[MAX_TREES];
 Node* NODES[MAX_TREES * MAX_NODES];
 
+bool recalculate(Node &node) {
+    if (is_leaf(node)) {
+        node.d_length = 1;
+        node.d_sum = node.value;
+        node.d_product = node.value;
+    } else {
+        node.d_sum = 0;
+        node.d_product = 1;
+        node.d_length = 0;
+        node.children_number = 0;
+        
+        Node* son = node.first_son;
+        if (son != nullptr) {
+            if (!son->hidden) {
+                recalculate(*son);
+                node.d_sum += son->d_sum;
+                node.d_product += son->d_product;
+                node.d_length += son->d_length;
+                node.children_number += 1;
+            }
+            son = son->brother;
+        }
+    }
+}
 
 void print_exp(Node &node) {
     if (node.value == -1) {
@@ -53,9 +86,12 @@ Tree* new_tree() {
     return new_tree;
 }
 
+inline Node* get_node(Tree &tree, int i) {
+    return NODES[MAX_NODES * tree.i];
+}
 void register_node(Tree &tree, Node &node) {
-    NODES[MAX_NODES * tree.i + tree.next_free] = &node;
-    tree.next_free++;
+    NODES[MAX_NODES * tree.i + tree.leafs] = &node;
+    tree.leafs++;
 }
 
 void add_child(Node &parent, Node &child) {
@@ -89,10 +125,40 @@ bool isomorphic(Node *first, Node *second) {
         return true;
     if (first == nullptr || second == nullptr)
         return false;
+    if (first->hidden && second->hidden)
+        return true;
+    if (first->hidden != second->hidden)
+        return false;
     return (first->children_number == second->children_number 
          && first->d_length == second->d_length 
          && first->d_sum == second->d_sum 
          && first->d_product == second->d_product);
+}
+
+
+bool combinations_of_k(int start, int k, Tree &tree1, Tree &tree2) {
+    if (k == 0) {
+        recalculate(*tree1.root);
+        recalculate(*tree2.root);
+        return isomorphic(tree1.root, tree2.root);
+    }
+    for (int i = start; i < tree1.leafs; i++) {
+        Node removed = *get_node(tree1, i);
+        removed.hidden = true;
+        if(combinations_of_k(i+1, k-1, tree1, tree2)) {
+            return true;
+        }
+        removed.hidden = false;
+    }
+    return false;
+}
+
+int progressing_powerset(Tree &tree1, Tree &tree2) {
+    for (int i = 1; i < tree1.leafs; i++) {
+        if (combinations_of_k(0, i, tree1, tree2)) {
+            return i;
+        }
+    }
 }
 
 void parse(string input, Node &parent, Tree &tree) {
@@ -155,21 +221,19 @@ void test() {
 }
 int main() {
     string input1 = "(4,(8),(2,((6))))";
-    // string input1 = "4,8,(2,6)";
     string input2 = "((8,((4)),(6,2)))";
-    // string input2 = "8,4,(6,2)";
-    Tree tree1 = *new_tree(1);
-    Tree tree2 = *new_tree(2);
+    Tree tree1 = *new_tree();
+    Tree tree2 = *new_tree();
     parse(input1, *tree1.root, tree1);
     parse(input2, *tree2.root, tree2);
 
-    print_exp(*tree1.root);
-    cout << endl << tree1.root->d_length ;
-    cout << endl << tree1.root->children_number;
-    cout << endl;
-    cout << endl << tree2.root->d_length;
-    cout << endl << tree2.root->children_number;
-    cout << endl ;
-    cout << endl << isomorphic(tree1.root, tree1.root) << endl;
+    // print_exp(*tree1.root);
+    // cout << endl << tree1.root->d_length ;
+    // cout << endl << tree1.root->children_number;
+    // cout << endl;
+    // cout << endl << tree2.root->d_length;
+    // cout << endl << tree2.root->children_number;
+    // cout << endl ;
+    cout << endl << progressing_powerset(tree1, tree2) << endl;
     return 0;
 }
